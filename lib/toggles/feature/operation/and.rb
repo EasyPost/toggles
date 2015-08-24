c766b6dbffef251bbf42497918ef92356cd91558
Andrew Tribone <atribone@gmail.com>
2015-08-24 16:21:36 -0700

    fix attribute handling for and

--- lib/toggles/feature/operation/and.rb
+++ lib/toggles/feature/operation/and.rb
@@ -3,7 +3,11 @@ module Feature
     class And
       def self.call(entity, attr_name, expected)
         expected.all? do |operation, value|
-          OPERATIONS[operation.to_sym].call(entity, attr_name, value)
+          if OPERATIONS.include? operation.to_sym
+            OPERATIONS[operation.to_sym].call(entity, attr_name, value)
+          else
+            Operation::Attribute.call(entity, operation, value)
+          end
         end
       end
     end
---
+++ spec/toggles/feature/acceptance/complex_and_spec.rb
@@ -0,0 +1,7 @@
+describe Feature::ComplexAnd do
+  specify do
+    expect(Feature::ComplexAnd.enabled_for?(data: double(id: 1))).to eq true
+    expect(Feature::ComplexAnd.enabled_for?(data: double(id: 2, timestamp: 0))).to eq true
+    expect(Feature::ComplexAnd.enabled_for?(data: double(id: 2, timestamp: 20))).to eq false
+  end
+end
