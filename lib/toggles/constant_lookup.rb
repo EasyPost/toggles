ffb67641d460d0c4173d85876ae3f3346447bb2f
Joshua Lane <joshlane@easypost.com>
2021-04-12 16:44:10 +0000

    fix: cut a new class everytime to be thread safe

--- lib/toggles/constant_lookup.rb
+++ lib/toggles/constant_lookup.rb
@@ -8,28 +8,27 @@ class Feature::ConstantLookup
     end
   end
 
-  Resolver = Class.new do
-    class << self
-      attr_accessor :features
-      attr_accessor :path
-
-      def const_missing(sym)
-        subtree_or_feature = features.fetch(
-          sym.to_s.gsub(/([a-z])([A-Z])/) { |s| s.chars[0] + "_" + s.chars[1] }.downcase.to_sym,
-        )
-        if subtree_or_feature.is_a?(Hash)
-          Feature::ConstantLookup.from(subtree_or_feature, path + [sym])
-        else
-          subtree_or_feature
-        end
-      rescue KeyError
-        raise Error.new(path + [sym])
-      end
-    end
-  end
 
   def self.from(features, path)
-    Resolver.tap do |resolver|
+    Class.new {
+      class << self
+        attr_accessor :features
+        attr_accessor :path
+
+        def const_missing(sym)
+          subtree_or_feature = features.fetch(
+            sym.to_s.gsub(/([a-z])([A-Z])/) { |s| s.chars[0] + "_" + s.chars[1] }.downcase.to_sym,
+          )
+          if subtree_or_feature.is_a?(Hash)
+            Feature::ConstantLookup.from(subtree_or_feature, path + [sym])
+          else
+            subtree_or_feature
+          end
+        rescue KeyError
+          raise Error.new(path + [sym])
+        end
+      end
+    }.tap do |resolver|
       resolver.features = features
       resolver.path = path
     end
