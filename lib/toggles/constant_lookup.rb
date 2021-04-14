5d347d1bed31112a229c2142652a40867349ad3b
Joshua Lane <joshlane@easypost.com>
2021-04-14 03:04:00 +0000

    doc: context for less obvious functions

--- lib/toggles/constant_lookup.rb
+++ lib/toggles/constant_lookup.rb
@@ -9,6 +9,16 @@ class Feature::ConstantLookup
   end
 
 
+  # Return a tree walker that translates Module#const_missing(sym) into the next child node
+  #
+  # So Features::Cat::BearDog walks as:
+  # * next_features = Feature.features # root
+  # * const_missing(:Cat) => next_features = next_features['cat']
+  # * const_missing(:BearDog) => next_features['bear_dog']
+  #
+  # Defined at Toggles.features_dir + "/cat/bear_dog.yaml"
+  #
+  # @raise [Error] if constant cannot be resoled
   def self.from(features, path)
     Class.new {
       class << self
@@ -17,6 +27,7 @@ class Feature::ConstantLookup
 
         def const_missing(sym)
           subtree_or_feature = features.fetch(
+            # translate class name into path part i.e :BearDog #=> 'bear_dog'
             sym.to_s.gsub(/([a-z])([A-Z])/) { |s| s.chars.join('_') }.downcase.to_sym,
           )
           if subtree_or_feature.is_a?(Hash)
