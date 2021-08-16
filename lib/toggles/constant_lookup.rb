da1d8c86f08177d600055f839f184a720df4dcb9
Joshua Lane <joshlane@easypost.com>
2021-08-16 14:04:27 -0700

    fix: handle abbreviations and numbers in feature name

--- lib/toggles/constant_lookup.rb
+++ lib/toggles/constant_lookup.rb
@@ -8,13 +8,13 @@ class Feature::ConstantLookup
     end
   end
 
-
   # Return a tree walker that translates Module#const_missing(sym) into the next child node
   #
   # So Features::Cat::BearDog walks as:
   # * next_features = Feature.features # root
   # * const_missing(:Cat) => next_features = next_features['cat']
   # * const_missing(:BearDog) => next_features['bear_dog']
+  # * const_missing(:CN22) => next_features['c_n_22']
   #
   # Defined at Toggles.features_dir + "/cat/bear_dog.yaml"
   #
@@ -22,21 +22,28 @@ class Feature::ConstantLookup
   def self.from(features, path)
     Class.new {
       class << self
-        attr_accessor :features
-        attr_accessor :path
+        attr_accessor :features, :path
 
         def const_missing(sym)
-          subtree_or_feature = features.fetch(
-            # translate class name into path part i.e :BearDog #=> 'bear_dog'
-            sym.to_s.gsub(/([a-z])([A-Z])/) { |s| s.chars.join('_') }.downcase.to_sym,
-          )
+          # translate class name into path part i.e :BearDog #=> 'bear_dog'
+          key =
+            sym
+            .to_s
+            .gsub(/([A-Z]{2,})/) { |s| s.chars.join('_') }
+            .gsub(/([a-z])([A-Z])/) { |s| s.chars.join('_') }
+            .downcase
+            .gsub(/([a-z])([0-9])/) { |s| s.chars.join('_') }
+            .to_sym
+
+          subtree_or_feature = features.fetch(key)
+
           if subtree_or_feature.is_a?(Hash)
             Feature::ConstantLookup.from(subtree_or_feature, path + [sym])
           else
             subtree_or_feature
           end
         rescue KeyError
-          raise Error.new(path + [sym])
+          raise Error, path + [sym]
         end
       end
     }.tap do |resolver|
--- spec/toggles/feature/base_spec.rb
+++ spec/toggles/feature/base_spec.rb
@@ -2,9 +2,19 @@ describe Feature::Base do
   let(:user)   { double(id: 1, logged_in?: true) }
   let(:widget) { double(id: 2) }
 
-  subject { Feature::MultipleSubjects.new(user: user, widget: widget) }
+  context 'multiple subjects' do
+    subject { Feature::MultipleSubjects.new(user: user, widget: widget) }
 
-  its(:enabled?)              { is_expected.to eq true }
-  its(:subjects)              { is_expected.to eq user: user, widget: widget }
-  its("permissions.subjects") { is_expected.to eq [:user, :widget] }
+    its(:enabled?)              do is_expected.to eq true end
+    its(:subjects)              do is_expected.to eq user: user, widget: widget end
+    its('permissions.subjects') { is_expected.to eq %i[user widget] }
+  end
+
+  context 'irregular capitalization' do
+    subject { Feature::AbbreviationsCN22.new(user: user) }
+
+    its(:enabled?)              do is_expected.to eq true end
+    its(:subjects)              do is_expected.to eq user: user end
+    its('permissions.subjects') { is_expected.to eq [:user] }
+  end
 end
